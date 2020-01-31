18b09cbee7ba440d39015a0f8c1c71cfb7b289c3
808278 <808278@SAIT224295IT.ACDM.DS.SAIT.CA>
2020-01-31 10:51:28 -0700

    Edited Book class Added CookBook and ChildrenBook classes

--- CPRG251_A2_Kim_Thai/src/sait/bms/problemdomain/Book.java
+++ CPRG251_A2_Kim_Thai/src/sait/bms/problemdomain/Book.java
@@ -1,6 +1,90 @@
 package sait.bms.problemdomain;
 
 public class Book {
-	private String title;
 	private long ISBN;
+	private double callNumber;
+	private int available;
+	private int total;
+	private String title;
+	
+	
+	/**
+	 * @param iSBN
+	 * @param callNumber
+	 * @param available
+	 * @param total
+	 * @param title
+	 */
+	public Book(long iSBN, double callNumber, int available, int total, String title) {
+		super();
+		ISBN = iSBN;
+		this.callNumber = callNumber;
+		this.available = available;
+		this.total = total;
+		this.title = title;
+	}
+	
+	
+	/**
+	 * @return the iSBN
+	 */
+	public long getISBN() {
+		return ISBN;
+	}
+	/**
+	 * @return the callNumber
+	 */
+	public double getCallNumber() {
+		return callNumber;
+	}
+	/**
+	 * @return the available
+	 */
+	public int getAvailable() {
+		return available;
+	}
+	/**
+	 * @return the total
+	 */
+	public int getTotal() {
+		return total;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param iSBN the iSBN to set
+	 */
+	public void setISBN(long iSBN) {
+		ISBN = iSBN;
+	}
+	/**
+	 * @param callNumber the callNumber to set
+	 */
+	public void setCallNumber(double callNumber) {
+		this.callNumber = callNumber;
+	}
+	/**
+	 * @param available the available to set
+	 */
+	public void setAvailable(int available) {
+		this.available = available;
+	}
+	/**
+	 * @param total the total to set
+	 */
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
 }
